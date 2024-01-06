cb42260b0ae29c213f85cea9f363c873f80596f5
Sayuri Custodio <sayuricustodio06@gmail.com>
2024-01-06 18:07:00 -0500

    bd nuevo ultimo

--- bd.sql
+++ bd.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS bd;
-USE bd;
+CREATE SCHEMA IF NOT EXISTS BD;
+USE BD;
 
 DROP TABLE IF EXISTS n_cliente;
 CREATE TABLE n_cliente 
  (id                         VARCHAR(3)  NOT NULL,
-  nombre                     VARCHAR(20) NOT NULL,
+  nombre                     VARCHAR(30) NOT NULL,
   telefono                   VARCHAR(20) NOT NULL,
   direccion                  VARCHAR(20),
   distrito                   VARCHAR(20),
